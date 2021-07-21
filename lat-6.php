3947d87cfa67dfa606ae9b24cd9262039e9fcd00
Nata <natajaya@gmail.com>
2021-07-21 15:17:52 +0700

    OK

--- lat-6.php
+++ lat-6.php
@@ -1,30 +1,5 @@
 <?php
-/*
-function bubbleSort($data)
-{
-    $swapped=true;
-    $n=count($data);
-    $temp=null;
-    while($swapped)//outer loop
-    {
-        $swapped = false;
-        for($i=0; $i<$n-1; $i++)//inner loop
-        {
-            if( $data[$i]>$data[$i+1]) //swap if the current value is greater the next value. change > to > for descending order
-            {
-                $temp=$data[$i];
-                $data[$i]=$data[$i+1];
-                $data[$i+1]=$temp;
-                $swapped=true;
-            }
-        }
-    }
- 
-    return $data;
-}
-*/
-
-$data = array (4, 2, 100, 143, 94, 7, 10, 55, 9, 48, 36);
+$data = array (4, 2, 100, 143, 94, 7, 10, 55, 9, 48, 36, 3);
 echo "Sebelum : " . implode (", ", $data) . "<br/>";
 $tukar = true;
 $jumlah = count($data);
