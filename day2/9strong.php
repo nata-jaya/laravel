ebfeb17ad447555782718f67b984984a5e24d4dc
Nata <natajaya@gmail.com>
2021-07-23 22:45:40 +0700

    add regex \.

--- day2/9strong.php
+++ day2/9strong.php
@@ -6,7 +6,7 @@
 <?php
 if (!empty($_POST['username']))
 {
-    if (preg_match("/^.*(?=.{4,10})(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[\-\_]).*$/", $_POST['username'])){
+    if (preg_match("/^.*(?=.{4,10})(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[\-\_\.]).*$/", $_POST['username'])){
     echo "username valid";
     } else {
     echo "username tidak valid";
