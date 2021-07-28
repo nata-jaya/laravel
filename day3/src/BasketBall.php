604e2de5a95837050f2494456ec763bd62a3ec97
Nata <natajaya@gmail.com>
2021-07-28 13:33:34 +0700

    added vendor

--- day3/src/BasketBall.php
+++ day3/src/BasketBall.php
@@ -4,5 +4,20 @@ namespace App;
 
 class BasketBall
 {
+    public $home;
+    public $away;
+
+    public function __construct(){
+        $this->home = $home;
+        $this->away = $away;
+    }
+
+    public function home(){
+        return $this->home;
+    }
+
+    public function away() {
+        return $this->away;
+    }
 
 }
